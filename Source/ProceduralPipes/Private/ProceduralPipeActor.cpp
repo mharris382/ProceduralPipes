96b18ecbf1d7b9c0387440df186d2dfdad4bc090
Max H <mharris382@gmail.com>
2025-12-22 10:40:41 -0500

    updated uplugin file, added copywrite, added batching example

--- Source/ProceduralPipes/Private/ProceduralPipeActor.cpp
+++ Source/ProceduralPipes/Private/ProceduralPipeActor.cpp
@@ -1,4 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright (c) 2025 Max Harris
+// Published by Procedural Architect
 
 
 #include "ProceduralPipeActor.h"
--- Source/ProceduralPipes/Public/ProceduralPipes.h
+++ Source/ProceduralPipes/Public/ProceduralPipes.h
@@ -1,4 +1,5 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright (c) 2025 Max Harris
+// Published by Procedural Architect
 
 #pragma once
 
